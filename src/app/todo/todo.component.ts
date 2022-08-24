1deee7725e1c18e1252597b24ddb0f8628533fd3
gucal <gucalaliriza@outlook.com>
2022-08-24 14:24:08 +0300

    todo css add | todo list add

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,11 +9,29 @@ import { Todo } from './todo';
 export class TodoComponent implements OnInit {
   constructor() {}
 
-  todo: Todo = {
-    id: 1,
-    text: 'Angular ogren',
-    checked: false,
-  };
+  todos: Todo[] = [
+    {
+      id: 1,
+      text: 'Angular ogren',
+      checked: false,
+    },
+    {
+      id: 2,
+      text: 'Angular todo ekle',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: 'Angular todo sil',
+      checked: true,
+    },
+  ];
+
+  selectedTodo?: Todo;
+
+  onSelect(selectTodo: Todo): void {
+    this.selectedTodo = selectTodo;
+  }
 
   ngOnInit(): void {}
 }
